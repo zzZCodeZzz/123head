e6231a40cd93e26aa8e3684ec474523a0af2abaa
manu <zach@devconsultberlin.de>
2021-09-25 18:08:30 +0200

    team picker under construction

--- src/admin/tournaments/TeamsPreview.tsx
+++ src/admin/tournaments/TeamsPreview.tsx
@@ -1,11 +1,25 @@
 import React, {FC} from "react";
 import {PlayerPreview} from "../users/PlayerPreview";
+import {Player} from "./redux/types";
+import _ from "lodash";
+
+const drawTeams = (players: Player[], size: number): Player[][] => {
+    const randomPlayers = _.shuffle(players);
+    const teams: Player[][] = [];
+    let currentTeamIndex = 0;
+    for (let i = 0; i < randomPlayers.length; i++) {
+        if (teams[currentTeamIndex].length < size || i === randomPlayers.length - 1) {
+            teams[currentTeamIndex].push(randomPlayers[i]);
+        }
+    }
+    return teams;
+}
 
 export const TeamsPreview: FC = () => {
-  return (
-      <div style={{display: "flex"}}>
-          <PlayerPreview displayName={"max"}/>
-          <PlayerPreview displayName={"moritz"}/>
-      </div>
-  );
+    return (
+        <div style={{display: "flex"}}>
+            <PlayerPreview displayName={"max"}/>
+            <PlayerPreview displayName={"moritz"}/>
+        </div>
+    );
 };
